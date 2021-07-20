48380eb128c763f6e95c705ad25941b5eb64b538
E <ethan@bouncebot.io>
2021-07-20 10:51:49 +0100

    Completed chapter 13

--- part-2/lists/tests/test_models.py
+++ part-2/lists/tests/test_models.py
@@ -1,4 +1,4 @@
-
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 from lists.models import Item, List
 
@@ -9,12 +9,12 @@ class ListAndItemModelsTest(TestCase):
         list_.save()
 
         first_item = Item()
-        first_item.text = 'The first (ever) list item'
+        first_item.text = "The first (ever) list item"
         first_item.list = list_
         first_item.save()
 
         second_item = Item()
-        second_item.text = 'Item the second'
+        second_item.text = "Item the second"
         second_item.list = list_
         second_item.save()
 
@@ -26,7 +26,18 @@ class ListAndItemModelsTest(TestCase):
 
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(first_saved_item.text, "The first (ever) list item")
         self.assertEqual(first_saved_item.list, list_)
-        self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(second_saved_item.text, "Item the second")
         self.assertEqual(second_saved_item.list, list_)
+
+    def test_cannot_save_empty_list_items(self):
+        list_ = List.objects.create()
+        item = Item(list=list_, text="")
+        with self.assertRaises(ValidationError):
+            item.save()
+            item.full_clean()
+
+    def test_get_absolute_url(self):
+        list_ = List.objects.create()
+        self.assertEqual(list_.get_absolute_url(), f"/lists/{list_.id}/")
